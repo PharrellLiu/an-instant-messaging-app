71011f25014d6af258ed40470e448c41cdc036cc
XINYUAN LIU <53179832+PharrellLiu@users.noreply.github.com>
2020-07-18 14:57:47 +0800

    u

---
+++ client3/MyApplication/app/src/main/java/my/application/MyApp.java
@@ -0,0 +1,25 @@
+package my.application;
+
+import android.app.Application;
+
+import org.xutils.x;
+
+public class MyApp extends Application {
+    private String name;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        x.Ext.init(this);
+        x.Ext.setDebug(false);
+        setName("");
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+
+}
