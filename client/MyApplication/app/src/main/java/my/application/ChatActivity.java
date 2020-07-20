585df4ad1d5e36f02e72f7f64d9e4f7961a474d1
XINYUAN LIU <53179832+PharrellLiu@users.noreply.github.com>
2020-07-20 17:08:13 +0800

    update

--- client/MyApplication/app/src/main/java/my/application/ChatActivity.java
+++ client/MyApplication/app/src/main/java/my/application/ChatActivity.java
@@ -3,16 +3,41 @@ package my.application;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.footer.ClassicsFooter;
+import com.scwang.smartrefresh.layout.header.ClassicsHeader;
+import com.scwang.smartrefresh.layout.listener.OnLoadMoreListener;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 
 import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ContentView(R.layout.activity_chat)
 public class ChatActivity extends AppCompatActivity {
 
+    @ViewInject(R.id.chat_recycler_view)
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<ChatMessageAdapter.ChatMessage> myDataset = new ArrayList<ChatMessageAdapter.ChatMessage>();
+    @ViewInject(R.id.chat_refreshLayout)
+    RefreshLayout refreshLayout;
+    @ViewInject(R.id.edittext_chat)
+    private EditText input_edittext;
+    @ViewInject(R.id.button_send)
+    private Button button_send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +47,38 @@ public class ChatActivity extends AppCompatActivity {
         int isChatroom = intent.getIntExtra("isChatroom",-1);
         String nameOfChatroomOrFri = intent.getStringExtra("nameOfChatroomOrFri");
         this.setTitle(nameOfChatroomOrFri);
+
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setHomeButtonEnabled(true);
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        recyclerView.setHasFixedSize(true);
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        mAdapter = new ChatMessageAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
+
+        refreshLayout.setRefreshHeader(new ClassicsHeader(this));
+        refreshLayout.setRefreshFooter(new ClassicsFooter(this));
+        refreshLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(RefreshLayout refreshlayout) {
+            }
+        });
+        refreshLayout.setOnLoadMoreListener(new OnLoadMoreListener() {
+            @Override
+            public void onLoadMore(RefreshLayout refreshlayout) {
+            }
+        });
+
+        myDataset.add(new ChatMessageAdapter.ChatMessage("root","2015","test",1));
+        myDataset.add(new ChatMessageAdapter.ChatMessage("lbw","2015","test",0));
+        myDataset.add(new ChatMessageAdapter.ChatMessage("root","2015","test",1));
+        myDataset.add(new ChatMessageAdapter.ChatMessage("lbw","2015","test",0));
+        mAdapter.notifyDataSetChanged();
 
     }
 
---
+++ client/MyApplication/app/src/main/java/my/application/ChatMessageAdapter.java
@@ -0,0 +1,117 @@
+package my.application;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ChatMessageAdapter extends RecyclerView.Adapter<ChatMessageAdapter.ChatMessageViewHolder>{
+
+    private List<ChatMessage> mDataset;
+
+
+    public ChatMessageAdapter(List<ChatMessage> myDataset){
+        mDataset = myDataset;
+    }
+
+    @NonNull
+    @Override
+    public ChatMessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v =  LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.chat_message_recyclerview, parent, false);
+        ChatMessageViewHolder vh = new ChatMessageViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChatMessageViewHolder holder, int position) {
+        ChatMessage chatMessage = mDataset.get(position);
+        int type = chatMessage.getType();
+        if (type == 1) { // send
+            holder.receive_layout.setVisibility(View.GONE);
+            holder.textview_receive_name.setVisibility(View.GONE);
+            holder.textview_receive_time.setVisibility(View.GONE);
+            holder.textview_receive_message.setVisibility(View.GONE);
+
+            holder.send_layout.setVisibility(View.VISIBLE);
+            holder.textview_send_name.setVisibility(View.VISIBLE);
+            holder.textview_send_time.setVisibility(View.VISIBLE);
+            holder.textview_send_message.setVisibility(View.VISIBLE);
+
+            holder.textview_send_name.setText(chatMessage.getName());
+            holder.textview_send_time.setText(chatMessage.getTime());
+            holder.textview_send_message.setText(chatMessage.getMessage());
+
+        } else { // receive
+            holder.receive_layout.setVisibility(View.VISIBLE);
+            holder.textview_receive_name.setVisibility(View.VISIBLE);
+            holder.textview_receive_time.setVisibility(View.VISIBLE);
+            holder.textview_receive_message.setVisibility(View.VISIBLE);
+
+            holder.send_layout.setVisibility(View.GONE);
+            holder.textview_send_name.setVisibility(View.GONE);
+            holder.textview_send_time.setVisibility(View.GONE);
+            holder.textview_send_message.setVisibility(View.GONE);
+
+            holder.textview_receive_name.setText(chatMessage.getName());
+            holder.textview_receive_time.setText(chatMessage.getTime());
+            holder.textview_receive_message.setText(chatMessage.getMessage());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+    public static class ChatMessage {
+        // TYPE_RECEIVED = 0
+        // TYPE_SEND = 1
+        private int type;
+        private String name;
+        private String time;
+        private String message;
+
+        public ChatMessage(String name, String time, String message, int type) {
+            this.message = message;
+            this.type = type;
+            this.name = name;
+            this.time = time;
+        }
+
+        public String getName(){return name;}
+        public String getTime(){return time;}
+        public String getMessage(){return message;}
+        public int getType(){return type;}
+    }
+
+    public static class ChatMessageViewHolder extends RecyclerView.ViewHolder {
+        public LinearLayout send_layout;
+        public LinearLayout receive_layout;
+        public TextView textview_receive_name;
+        public TextView textview_receive_time;
+        public TextView textview_receive_message;
+        public TextView textview_send_name;
+        public TextView textview_send_time;
+        public TextView textview_send_message;
+
+        public ChatMessageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            send_layout = itemView.findViewById(R.id.send_layout);
+            receive_layout = itemView.findViewById(R.id.receive_layout);
+            textview_receive_name = itemView.findViewById(R.id.textview_receive_name);
+            textview_receive_time = itemView.findViewById(R.id.textview_receive_time);
+            textview_receive_message = itemView.findViewById(R.id.textview_receive_message);
+            textview_send_name = itemView.findViewById(R.id.textview_send_name);
+            textview_send_time = itemView.findViewById(R.id.textview_send_time);
+            textview_send_message = itemView.findViewById(R.id.textview_send_message);
+        }
+    }
+
+}
