02cdcd18724c8843c124f833a51b913269769fb8
XINYUAN LIU <53179832+PharrellLiu@users.noreply.github.com>
2020-07-21 13:31:36 +0800

    Update URLCollection.java

--- client/MyApplication/app/src/main/java/my/application/URLCollection.java
+++ client/MyApplication/app/src/main/java/my/application/URLCollection.java
@@ -1,13 +1,13 @@
 package my.application;
 
 public class URLCollection {
-    public static final String URL = "http://192.168.0.102:5000/api";
+    public static final String URL = "http://192.168.0.100:5000/api";
     public static final String LOGIN = URL + "/login";
     public static final String REGISTER = URL + "/register";
     public static final String GET_FRI_LIST = URL + "/get_fri_list";
     public static final String GET_CHATROOM_LIST = URL + "/get_chatroom_list?name=";
-    public static final String GET_CHATROOM_MESSAGES = URL + "/get_chatroom_messages?page=";
-    public static final String GET_PRIVATE_CHAT_MESSAGES = URL + "/get_private_chat_messages?page=";
+    public static final String GET_CHATROOM_MESSAGES = URL + "/get_chatroom_messages";
+    public static final String GET_PRIVATE_CHAT_MESSAGES = URL + "/get_private_chat_messages";
     public static final String POST_PRIVATE_CHAT_MESSAGE = URL + "/post_private_chat_message";
     public static final String POST_CHATROOM_MESSAGE = URL + "/post_chatroom_message";
 }
