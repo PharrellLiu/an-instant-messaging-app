65f51e874fce4eaac702c67b01cf0edb6c2aa0d2
XINYUAN LIU <53179832+PharrellLiu@users.noreply.github.com>
2020-07-21 13:38:13 +0800

    Update HomeActivity.java

--- client/MyApplication/app/src/main/java/my/application/HomeActivity.java
+++ client/MyApplication/app/src/main/java/my/application/HomeActivity.java
@@ -17,8 +17,6 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_chatroom, R.id.navigation_private_chat, R.id.navigation_moment)
                 .build();
