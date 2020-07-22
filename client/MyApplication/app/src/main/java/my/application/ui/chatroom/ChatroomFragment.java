74265c5c242f38c8d1a01219f5a82a04cbcae6a3
XINYUAN LIU <53179832+PharrellLiu@users.noreply.github.com>
2020-07-22 12:09:56 +0800

    client up

--- client/MyApplication/app/src/main/java/my/application/ui/chatroom/ChatroomFragment.java
+++ client/MyApplication/app/src/main/java/my/application/ui/chatroom/ChatroomFragment.java
@@ -1,18 +1,8 @@
 package my.application.ui.chatroom;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.lifecycle.ViewModelProviders;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -20,6 +10,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
