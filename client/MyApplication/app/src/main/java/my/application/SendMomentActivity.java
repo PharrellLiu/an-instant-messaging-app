f4102f2ff2e9c53b5572c067498b7b9fa8d5258a
XINYUAN LIU <53179832+PharrellLiu@users.noreply.github.com>
2020-07-27 18:47:30 +0800

    client update

--- client/MyApplication/app/src/main/java/my/application/SendMomentActivity.java
+++ client/MyApplication/app/src/main/java/my/application/SendMomentActivity.java
@@ -1,17 +1,45 @@
 package my.application;
 
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.luck.picture.lib.PictureSelector;
+import com.luck.picture.lib.config.PictureConfig;
+import com.luck.picture.lib.config.PictureMimeType;
+import com.luck.picture.lib.entity.LocalMedia;
+
 import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
+
+import java.util.List;
 
 @ContentView(R.layout.activity_send_moment)
 public class SendMomentActivity extends AppCompatActivity {
+
+    @ViewInject(R.id.input_moment_content)
+    private EditText input_moment_content;
+    @ViewInject(R.id.choose_picture)
+    private TextView choose_picture;
+    @ViewInject(R.id.picture_chosen)
+    private ImageView picture_chosen;
+
+    private String mediaURl;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +52,54 @@ public class SendMomentActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
+        choose_picture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PictureSelector.create(SendMomentActivity.this)
+                        .openGallery(PictureMimeType.ofAll())
+                        .loadImageEngine(GlideEngine.createGlideEngine())
+                        .isCamera(true)
+                        .isWeChatStyle(true)
+                        .previewImage(true)
+                        .previewVideo(true)
+                        .previewEggs(true)
+                        .maxSelectNum(1)
+                        .isMaxSelectEnabledMask(true)
+                        .forResult(PictureConfig.CHOOSE_REQUEST);
+            }
+        });
 
+    }
+
+    public static String getRealPathFromURI(Context context, Uri contentURI) {
+        String result;
+        Cursor cursor = context.getContentResolver().query(contentURI,
+                new String[]{MediaStore.Images.ImageColumns.DATA},//
+                null, null, null);
+        if (cursor == null) result = contentURI.getPath();
+        else {
+            cursor.moveToFirst();
+            int index = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            result = cursor.getString(index);
+            cursor.close();
+        }
+        return result;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == PictureConfig.CHOOSE_REQUEST) {
+                List<LocalMedia> selectList = PictureSelector.obtainMultipleResult(data);
+                mediaURl = selectList.get(0).getPath();
+                Glide.with(this)
+                        .load(selectList.get(0).getPath())
+                        .into(picture_chosen);
+                Uri uri = Uri.parse((String) mediaURl);
+                mediaURl = getRealPathFromURI(this, uri);
+            }
+        }
     }
 
     @Override
@@ -35,9 +110,14 @@ public class SendMomentActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            this.finish();
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                break;
+            case R.id.action_send_moment:
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
+
 }
