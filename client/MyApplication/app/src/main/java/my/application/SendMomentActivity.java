52c413a13a8145bb2bc34286ac5db62f53f04d83
XINYUAN LIU <53179832+PharrellLiu@users.noreply.github.com>
2020-07-27 16:53:30 +0800

    Update SendMomentActivity.java

--- client/MyApplication/app/src/main/java/my/application/SendMomentActivity.java
+++ client/MyApplication/app/src/main/java/my/application/SendMomentActivity.java
@@ -1,14 +1,43 @@
 package my.application;
 
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import org.xutils.view.annotation.ContentView;
+import org.xutils.x;
 
+@ContentView(R.layout.activity_send_moment)
 public class SendMomentActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_send_moment);
+        x.view().inject(SendMomentActivity.this);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.send_moment_actionbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
